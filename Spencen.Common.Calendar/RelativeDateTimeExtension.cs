7128ec580776830b956d76d18881dcd893c92c85
Nigel Spencer <nigel@spencen.com>
2020-07-20 00:14:47 -0400

    Add RelativeDate and unit tests.

---
+++ Spencen.Common.Calendar/RelativeDateTimeExtension.cs
@@ -0,0 +1,13 @@
+﻿namespace Spencen.Common.Calendar
+{
+    using System;
+
+    public static class RelativeDateTimeExtension
+    {
+        public static DateTime RelativeDate(this DateTime asOf, string relativeDateExpression)
+        {
+            var relativeDate = new RelativeDate(relativeDateExpression);
+            return relativeDate.Apply(asOf);
+        }
+    }
+}
